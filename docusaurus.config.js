8deef1f3b9dde79251ede6ad66cb7915cec35c5d
qinglian.li <qinglian.li@d-robotics.cc>
2026-06-03 18:45:28 +0800

    Update feedback links
    Signed-off-by: qinglian.li <qinglian.li@d-robotics.cc>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,10 +50,10 @@ const config = {
 
     feedbackFloat: {
       enabled: true,
-          questionnaireUrl: "https://horizonrobotics.feishu.cn/wiki/EZs4w6IxMixCDbklSuvcYHhtnaf",
+          questionnaireUrl: "https://horizonrobotics.feishu.cn/share/base/form/shrcnLQ9OfYQO03cebdkNfOmkCh",
           questionnaireUrlByLocale: {
-            "zh-Hans": "https://horizonrobotics.feishu.cn/wiki/EZs4w6IxMixCDbklSuvcYHhtnaf?table=tblZSK5RpDLRZkad&view=vewpz6d6Un",
-            en: "https://horizonrobotics.feishu.cn/wiki/EZs4w6IxMixCDbklSuvcYHhtnaf?table=tblchS7yyQojb6b9&view=vewpz6d6Un",
+            "zh-Hans": "https://horizonrobotics.feishu.cn/share/base/form/shrcnLQ9OfYQO03cebdkNfOmkCh",
+            en: "https://horizonrobotics.feishu.cn/share/base/form/shrcnLQ9OfYQO03cebdkNfOmkCh",
       },
       // 站点内路径规则（基于 baseUrl 之后的路径）：
       // - "/*" 匹配全部页面（中英文均生效）
